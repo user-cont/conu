3744bbf62c230bad9929908c5dfde6017df8abe8
Tomas Tomecek <ttomecek@redhat.com>
2017-09-25 17:37:18 +0200

    better logging
    * per-module logger
    * configurable library-specific logger via Backend interface
    * unify logging messages (lower-case start)
    
    Fixes #7
    
    Built on top of #21, will rebase once merged
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- conu/apidefs/backend.py
+++ conu/apidefs/backend.py
@@ -1,9 +1,49 @@
 """
-Definition for a backend class
+Definition for a backend class and logging initialization
 """
+
+import logging
 
 from .container import Container
 from .image import Image
+
+
+__version__ = "0.0.1-alpha"
+
+
+def set_logging(
+        logger_name="conu",
+        level=logging.INFO,
+        handler_class=logging.StreamHandler,
+        handler_kwargs=None,
+        format='%(asctime)s.%(msecs).03d %(filename)-17s %(levelname)-6s %(message)s',
+        date_format='%H:%M:%S'):
+    """
+    Set personal logger for this library.
+
+    :param logger_name: str, name of the logger
+    :param level: int, see logging.{DEBUG,INFO,ERROR,...}: level of logger and handler
+    :param handler_class: logging.Handler instance, default is StreamHandler (/dev/stderr)
+    :param handler_kwargs: dict, keyword arguments to handler's constructor
+    :param format: str, formatting style
+    :param date_format: str, date style in the logs
+    :return: logger instance
+    """
+    logger = logging.getLogger(logger_name)
+    # do we want to propagate to root logger?
+    # logger.propagate = False
+    logger.setLevel(level)
+
+    handler_kwargs = handler_kwargs or {}
+    handler = handler_class(**handler_kwargs)
+    handler.setLevel(level)
+
+    formatter = logging.Formatter(format, date_format)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
 
 class Backend(object):
     """
@@ -12,3 +52,17 @@ class Backend(object):
 
     ContainerClass = Container
     ImageClass = Image
+
+    def __init__(self, logging_level=logging.INFO, logging_kwargs=None):
+        """
+        This method serves as a configuration interface for conu.
+
+        :param logging_level: int, control logger verbosity: see logging.{DEBUG,INFO,ERROR}
+        :param logging_kwargs: dict, additional keyword arguments for logger set up, for more info
+                                see docstring of set_logging function
+        """
+        self.logging_level = logging_level
+        logging_kwargs = logging_kwargs or {}
+        logger = set_logging(level=self.logging_level, **logging_kwargs)
+        logger.info("conu has initiated, welcome to the party!")
+        logger.debug("conu version: %s", __version__)
