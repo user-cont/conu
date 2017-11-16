d412915320594c3373acc69ee8eeb7df1c3c8cc8
Dominika Hodovska <dhodovsk@redhat.com>
2017-11-16 10:27:50 +0100

    Use python in s2i example

--- docs/source/examples/s2i.py
+++ docs/source/examples/s2i.py
@@ -1,8 +1,8 @@
 from conu.backend.docker.image import S2IDockerImage
 from conu.backend.docker.container import DockerContainer
 
-source = 'https://github.com/openshift/ruby-hello-world'
-image = S2IDockerImage("centos/ruby-23-centos7")
+source = 'https://github.com/dbarnett/python-helloworld'
+image = S2IDockerImage("centos/python-35-centos7")
 image.pull()
 extended_image = image.extend(source, "myapp")
 container = DockerContainer.run_via_binary(extended_image)
