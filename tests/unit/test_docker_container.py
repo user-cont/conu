eabc0bcc2d3b0dfca7dd5ed625eef540ee2b0112
Dominika Hodovska <dhodovsk@redhat.com>
2017-12-01 13:12:11 +0100

    method for getting published ports

--- tests/unit/test_docker_container.py
+++ tests/unit/test_docker_container.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, unicode_literals
 
-from conu import DockerRunCommand
+from conu import DockerRunCommand, DockerImage, DockerContainer
 
 
 def test_dr_command_class():
@@ -19,3 +19,40 @@ def test_dr_command_class():
     # test whether mutable params are not mutable across instances
     simple.options += ["spy"]
     assert "spy" not in DockerRunCommand().options
+
+
+def test_get_port_mappings():
+    image_name = "registry.fedoraproject.org/fedora"
+    image_tag = "27"
+    image = DockerImage(image_name, image_tag)
+
+    command = DockerRunCommand(additional_opts=["-p", "321:123"])
+
+    try:
+        image.get_metadata()
+    except Exception:
+        image.pull()
+
+    container = DockerContainer.run_via_binary(image, command)
+    try:
+        mappings = container.get_port_mappings(123)
+
+        assert len(mappings) == 1
+        assert mappings == [{"HostIp": '0.0.0.0', "HostPort": '321'}]
+
+        mappings = container.get_port_mappings()
+        assert len(mappings) == 1
+        assert mappings == {'123/tcp': [{'HostIp': '0.0.0.0', 'HostPort': '321'}]}
+    finally:
+        container.stop()
+        container.delete()
+
+    command = DockerRunCommand()
+    container = DockerContainer.run_via_binary(image, command)
+    try:
+        mappings = container.get_port_mappings(123)
+
+        assert not mappings
+    finally:
+        container.stop()
+        container.delete()
