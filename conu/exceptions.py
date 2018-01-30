b7de79baba92a724f54de12634dfbd142fb4de00
Dominika Hodovska <dhodovsk@redhat.com>
2018-01-30 16:42:06 +0100

    Add possibility to check signature

--- conu/exceptions.py
+++ conu/exceptions.py
@@ -3,3 +3,7 @@ from __future__ import print_function, unicode_literals
 
 class ConuException(Exception):
     """ Generic exception when something goes wrong """
+
+
+class PackageSignatureException(ConuException):
+    """ Exception raised when package signature validation goes wrong"""
---
+++ docs/source/examples/check_signature.py
@@ -0,0 +1,11 @@
+from conu import DockerBackend
+
+
+def check_signature(backend):
+    image = backend.ImageClass('docker.io/modularitycontainers/memcached')
+    image.pull()
+    image.has_pkgs_signed_with(['812a6b4b64dab85d'])
+
+
+with DockerBackend() as backend:
+    check_signature(backend)
