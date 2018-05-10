829ecf1f065586f040ecaf1aadd575bb8d9929dc
Radoslav Pitonak <rado.pitonak@gmail.com>
2018-05-10 13:14:10 +0100

    naming changes, enum for status

--- conu/apidefs/metadata.py
+++ conu/apidefs/metadata.py
@@ -1,13 +1,15 @@
-class Metadata:
+import enum
+
+
+class Metadata(object):
     """
     Common metadata for container and image
     """
 
-    def __init__(self, name=None, id=None, ports=None, labels=None, command=None,
+    def __init__(self, name=None, identifier=None, labels=None, command=None,
                  creation_timestamp=None, env_variables=None):
         self.name = name
-        self.id = id
-        self.ports = ports
+        self.identifier = identifier
         self.labels = labels
         self.command = command
         self.creation_timestamp = creation_timestamp
@@ -19,12 +21,13 @@ class ContainerMetadata(Metadata):
     Specific metadata for container
     """
 
-    def __init__(self, name=None, id=None, ports=None, labels=None, command=None,
+    def __init__(self, name=None, identifier=None, ports=None, labels=None, command=None,
                  creation_timestamp=None, env_variables=None,
                  image=None, hostname=None, ipv4=None, ipv6=None, status=None):
         super(ContainerMetadata, self).__init__(
-            name=name, id=id, ports=ports, labels=labels, command=command,
+            name=name, identifier=identifier, labels=labels, command=command,
             creation_timestamp=creation_timestamp, env_variables=env_variables)
+        self.ports = ports
         self.hostname = hostname
         self.image = image
         self.ipv4 = ipv4
@@ -37,12 +40,24 @@ class ImageMetadata(Metadata):
     Specific metadata for image
     """
 
-    def __init__(self, name=None, id=None, ports=None, labels=None, command=None, creation_timestamp=None,
-                 env_variables=None, tags=None, registry=None, entrypoint=None):
+    def __init__(self, name=None, identifier=None, ports=None, labels=None, command=None, creation_timestamp=None,
+                 env_variables=None, image_names=None):
         super(ImageMetadata, self).__init__(
-            name=name, id=id, ports=ports, labels=labels, command=command,
+            name=name, identifier=identifier, labels=labels, command=command,
             creation_timestamp=creation_timestamp, env_variables=env_variables)
+        self.ports = ports
+        self.image_names = image_names
 
-        self.tags = tags
-        self.registry = registry
-        self.entrypoint = entrypoint
+
+class ContainerStatus(enum.Enum):
+    """
+    This Enum defines the status of container
+
+    """
+    CREATED = 0
+    RESTARTING = 1
+    RUNNING = 2
+    REMOVING = 3
+    PAUSED = 4
+    EXITED = 5
+    DEAD = 6
