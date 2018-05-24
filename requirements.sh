ab1ef64653fec0c8601a0bc3e8815b547bd5f2b8
Dominika Hodovska <dhodovsk@redhat.com>
2018-05-24 16:14:19 +0200

    install pyxattr package accross fedora distros

--- requirements.sh
+++ requirements.sh
@@ -5,9 +5,13 @@ set -e
 # old s2i breaks tests & functionality
 dnf install -y acl docker libselinux-utils \
   source-to-image \
-  python3-pyxattr python2-pyxattr \
+  python3-pyxattr \
   python3-docker python2-docker \
   python3-six python2-six \
   python3-pip python2-pip \
   python3-pytest python2-pytest \
   python2-enum34
+
+
+# pyxattr has different naming in Fedora 27 and Fedora 28
+dnf install -y python2-pyxattr || dnf install -y pyxattr
