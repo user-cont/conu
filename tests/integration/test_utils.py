29caaa3188aa7436e38afc6e3ef5c0f236aa354d
Sumit Ghosh <sumit@sumit-ghosh.com>
2018-08-15 22:49:08 +0530

    Add integration test for run_cmd

--- tests/integration/test_utils.py
+++ tests/integration/test_utils.py
@@ -22,7 +22,7 @@ import pytest
 from conu import check_port, Probe
 from conu.utils import s2i_command_exists, getenforce_command_exists, \
     chcon_command_exists, setfacl_command_exists, command_exists, CommandDoesNotExistException, \
-    check_docker_command_works
+    check_docker_command_works, run_cmd
 
 
 def test_probes_port():
@@ -60,3 +60,20 @@ def test_command_exists():
 
 def test_check_docker():
     assert check_docker_command_works()
+
+
+def test_run_cmd():
+    ret = run_cmd(["sh", "-c", "for x in `seq 1 5`; do echo $x; sleep 1; done"])
+    assert not ret
+
+    ret = run_cmd(["sh", "-c", "for x in `seq 1 5`; do echo $x; sleep 1; done"],
+                  return_output=True)
+    assert ret == '1\n2\n3\n4\n5\n'
+
+    with pytest.raises(subprocess.CalledProcessError) as excinfo:
+        ret = run_cmd(["sh", "-c", "exit 5"])
+        assert not ret
+    assert 5 == excinfo.value.returncode
+
+    ret = run_cmd(["sh", "-c", "exit 5"], ignore_status=True)
+    assert 5 == ret
