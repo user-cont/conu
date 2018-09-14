b936393f63c9a37cf1ae58cd073071d8c5087c5d
Radoslav Pitonak <rado.pitonak@gmail.com>
2018-09-14 14:02:25 +0200

    install origin from github

--- test-requirements.sh
+++ test-requirements.sh
@@ -7,9 +7,9 @@ source /etc/os-release
 set -x
 
 if [ "${NAME}" == "Fedora" ]; then
-    dnf install -y docker origin make
+    dnf install -y docker make
 elif [ "${NAME}" == "CentOS Linux" ]; then
-    yum install -y docker origin make
+    yum install -y docker make
 fi
 
 setenforce 0
@@ -17,6 +17,9 @@ systemctl start docker
 touch /etc/docker/daemon.json
 echo '{"insecure-registries" : [ "172.30.0.0/16" ]}' > /etc/docker/daemon.json
 systemctl restart docker
+curl -Lo openshift.tar.gz https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-server-v3.9.0-191fece-linux-64bit.tar.gz
+tar -zxf openshift.tar.gz
+mv -v openshift-origin-server-v3.9.0-191fece-linux-64bit/* /sbin/
 oc cluster status || oc cluster up --version v3.9.0
 oc login -u system:admin
 oc adm policy add-role-to-user system:registry developer
